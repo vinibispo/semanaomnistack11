7b88b89e185accce8ce6bd940bdb01782dd56878
vinibispo <vini.bispo015@gmail.com>
2020-03-24 12:23:13 -0300

    It's working, but now I will be refactoring

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
-const {Router} = require('express')
-const routes = Router()
 const crypto = require('crypto')
+const {Router} = require('express')
+const connection = require('./database/index')
 
-routes.post('/ongs', (req, res)=>{
+const routes = Router()
+
+routes.get('/ongs', async(req, res)=>{
+    const ongs = await connection('ongs').select('*')
+    return res.json({ongs})
+})
+
+routes.post('/ongs', async(req, res)=>{
     const {name, email, uf, city, whatsapp} = req.body
     const id = crypto.randomBytes(4).toString('HEX')
-    console.log(data)
-    res.json({})
+    await connection('ongs').insert({id, name, email, city, uf,whatsapp })
+    res.json({id})
 })
 
 module.exports = routes
